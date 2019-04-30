bc6874139cb3c2ecc21dd3450ebf19c40e733b89
Igor Kravchenko <ikravchenko@smartru.com>
2019-04-30 09:45:05 +0300

    ch1

---
+++ ch1/import/MyClass.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass {
+	public static void main(String[] args) {
+		java.util.ArrayList<String> a = new java.util.ArrayList<String>();
+		a.add("lalla");
+		TreeSet<String> t = new TreeSet<String>();
+		t.add("ttt");
+		System.out.println(a);
+		System.out.println(t);
+	}
+}
